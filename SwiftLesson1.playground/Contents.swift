ceac792e58636841dd4cf6ab674f3533fd140b07
MakarovskyiDmytro/MakarovskyiDmytro/MakarovskyiDmytro/MakarovskyiDmytroMakarovskyiDmytro <makarovskid@gmail.com>
2021-08-02 13:07:29 +0300

    SwiftLesson1

---
+++ SwiftLesson1.playground/Contents.swift
@@ -0,0 +1,7 @@
+let title = "Student characteristics:"
+let firstName = "Dmytro"
+let secondName = "Makarovskyi"
+let height = 185
+let weight = 88
+print(title)
+print("First Name - \(firstName)\nSecond Name - \(secondName)\nHeight, cm - \(height)\nWeight, kg - \(weight)")
